b800582d7d1056ea2a06295920b7b80a38244c3a
karonka <karonka@abv.bg>
2015-06-08 17:37:50 +0300

    Fixed stuff

--- week1/1-Vector/Vector.cpp
+++ week1/1-Vector/Vector.cpp
@@ -4,84 +4,89 @@
 using namespace std;
 
 class Vector{
-	int size;
-	int capacity;
+	int sz;
+	int cap;
 	int* vec;
 	public:
 	Vector(){
-		size = 0;
-		capacity = 16;
-		vec = (int*) malloc(capacity*sizeof(int));
+		sz = 0;
+		cap = 16;
+		vec = (int*) malloc(cap*sizeof(int));
 	}
 	void insert(int index,int value){
-		if (index > size || index < 0){
+		if (index > sz || index < 0){
 			cout<<"Out of bounds!";
+			return;
 		}
-		if (size + 1 > capacity){
+		if (sz + 1 > cap){
 			expand();
+			insert(index,value);
 		}
 		else{
-			for (int i = size + 1; i >= index; i--){
+			for (int i = sz - 1; i >= index; i--){
 				vec[i+1] = vec[i];
 			}
 			vec[index] = value;
-			size++;
+			sz++;
 		}
 	}
 	void add(int value){
-		if (size + 1 > capacity){
+		if (sz + 1 > cap){
 			expand();
 		}
-		vec[size++] = value;
+		vec[sz++] = value;
 	}
 	int get(int index){
-		if (index < 0 || index >= size){
+		if (index < 0 || index >= sz){
 			cout<<"Out of bounds!";
 			return -1;
 		}
 		return vec[index];
 	}
-	int getSize(){
-		return size;
+	int size(){
+		return sz;
 	}
-	int getCapacity(){
-		return capacity;
+	int capacity(){
+		return cap;
 	}
 	void pop(){
-		size--;
+		sz--;
 	}
 	void deleteV (int index){
-		for (int i = index; i < size - 1; i++){
+		for (int i = index; i < sz - 1; i++){
 			vec[i] = vec[i+1];
 		}
-		size--;
-		if (size < capacity/4){
+		sz--;
+		if (sz < cap/4){
 			contract();
 		}
 	}
 	void expand(){
 		cout<<"expanding.."<<endl;
-		vec = (int*)realloc(vec,(capacity+(int)(0.5*capacity))*sizeof(int));
-		capacity+=(int)(0.5*capacity);
+		vec = (int*)realloc(vec,(cap+(int)(0.5*cap))*sizeof(int));
+		cap+=(int)(0.5*cap);
 	}
 	void contract(){
-		cout<<"contracting.. form "<<capacity <<" to "<<((int)(0.5*capacity))<<endl;
-		vec = (int*)realloc(vec,((int)(0.5*capacity))*sizeof(int));
-		capacity = (int)(0.5*capacity);
+		cout<<"attempt to contract"<<endl;
+		if(cap < 16) return; // да не остават < 8
+		cout<<"contracting.. form "<<cap<<" to "<<((int)(0.5*cap))<<endl;
+		vec = (int*)realloc(vec,((int)(0.5*cap))*sizeof(int));
+		cap = (int)(0.5*cap);
 	}
 };
 int main(){
 	// test
 	Vector v;
 	for(int i = 1; i <= 16; i++) v.add(i);
-	for(int i = 0; i < v.getSize(); i++) cout<<v.get(i)<<" ";
+	for(int i = 0; i < v.size(); i++) cout<<v.get(i)<<" ";
+	cout<<endl;
 	v.insert(5,5);
-	for(int i = 0; i < v.getSize(); i++) cout<<v.get(i)<<" ";
+	for(int i = 0; i < v.size(); i++) cout<<v.get(i)<<" ";
 	cout<<endl;
 	for(int i = 16; i >= 2; i--){
 		v.deleteV(i);
 	}
-	for(int i = 0; i < v.getSize(); i++) cout<<v.get(i)<<" ";
+	for(int i = 0; i < v.size(); i++) cout<<v.get(i)<<" ";
 	cout<<endl;
 	return 0;	
 }
