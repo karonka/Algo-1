8ff0138ab49669265927152a1cba3689527f280a
karonka <karonka@abv.bg>
2015-06-21 16:25:42 +0300

    hw

--- week2/6-K-Lists/k_lists.cpp
+++ week2/6-K-Lists/k_lists.cpp
@@ -1,4 +1,6 @@
+#include <iostream>
 #include <vector>
+#define INF 2147483647
 
 using namespace std;
 
@@ -6,13 +8,81 @@ class Node {
 public:
   int value;
   Node* next;
+  Node(int val = 0, Node* nxt = NULL):value(val),next(nxt){};
 };
+
+void swap(Node& a, Node& b){
+	Node tmp = a;
+	a = b;
+	b = tmp;
+}
 
 class KLists {
 public:
+	void make_heap(Node** seq, int size){
+		for(int i = size/2 - 1; i >= 0; i--){
+			heapify(seq, size, i);
+		}
+	}
+	void heapify(Node** seq, int size, int i){
+		int a,b;
+		while(i*2 + 1 < size){
+			if (i*2 + 1 < size) a = seq[i*2 + 1]->value;
+			if (i*2 + 2 < size) b = seq[i*2 + 2]->value;
+			else b = INF;
+			int min = a < b? i*2+1 : i*2+2;
+			if (seq[min]->value < seq[i]->value){
+				swap(seq[min],seq[i]);
+				i = min;
+			}
+			else return;
+		}
+	}
 
   // Merge K sorted lists.
-  Node merge(vector<Node> lists) {
-    // ...
-  }
+	Node merge(vector<Node>& lists) {
+		Node* heap[lists.size()];
+		Node* begin; 
+		Node* currNode; 
+		for(int i = 0; i < lists.size(); i++){
+			heap[i] = &lists[i];
+		}
+		make_heap(heap, lists.size());
+		currNode = begin = heap[0];
+		int hasMore = lists.size();
+		if(heap[0]->next != NULL){
+			heap[0] = heap[0]->next;
+		}else{
+			hasMore--;
+			swap(heap[0],heap[hasMore]);
+		}
+		while(hasMore){
+			heapify(heap, hasMore, 0);
+			currNode->next = heap[0];
+			currNode = heap[0];
+			if(heap[0]->next != NULL){
+				heap[0] = heap[0]->next;
+			}else{
+				hasMore--;
+				swap(heap[0],heap[hasMore]);
+			}
+		}
+		currNode->next = NULL;
+		return *begin;
+	}
 };
+
+int main(){
+	Node a5(1665,NULL),a4(233,&a5),a3(46,&a4),a2(3,&a3),a1(2,&a2);
+	Node b4(300,NULL),b3(234,&b4),b2(5,&b3),b1(0,&b2);
+	Node c5(111,NULL),c4(23,&c5),c3(22,&c4),c2(15,&c3),c1(2,&c2);
+	KLists ob;
+	vector<Node> lists = {a1,b1,c1};
+	Node node = ob.merge(lists);
+	Node* n = &node;
+	while(n != NULL){
+		cout<<n->value<<" ";
+		n = n->next;
+	}
+	return 0;
+}
--- week2/7-K-Min/k_min.cpp
+++ week2/7-K-Min/k_min.cpp
@@ -1,12 +1,63 @@
+#include <iostream>
 #include <vector>
+#define INF 2147483647
 
 using namespace std;
+void swap(int &a, int &b){
+	int tmp = a;
+	a = b;
+	b = tmp;
+}
 
 class KMin {
 public:
-
-  // Finds the k-th minimum element in an unsorted collection.
-  int kthMinimum(vector<int> numbers, int k) {
-    // ...
-  }
+	void make_heap(vector<int>& seq){
+		int size = seq.size();
+		for(int i = size/2 - 1; i >= 0; i--){
+			heapify(seq, size, i);
+		}
+	}
+	void heapify(vector<int>& seq, int size, int i){
+		int a,b;
+		while(i*2 + 1 < size){
+			if (i*2 + 1 < size) a = seq[i*2 + 1];
+			if (i*2 + 2 < size) b = seq[i*2 + 2];
+			else b = INF;
+			int min = a < b? i*2+1 : i*2+2;
+			if (seq[min] < seq[i]){
+				swap(seq[min],seq[i]);
+				i = min;
+			}
+			else return;
+		}
+	}
+	int kthMinimum(vector<int> numbers, int k) {
+		make_heap(numbers);
+		int size = numbers.size();
+		int curr = numbers[0];
+		int c = 1;
+ 		for (int i = 0; i < size && c < k; i++){
+			if(numbers[0] != curr){
+				curr = numbers[0];
+				c++;
+			}
+			swap(numbers[0], numbers[size - i - 1]);
+			heapify(numbers, size - i - 1, 0);
+		}
+		if(c == k) return curr;
+		return -1;
+	}
 };
+
+int main(){
+	vector<int> arr = {0,5,3,6,4,8,1};
+	vector<int> arr2 = {10,15,3,6,4,6,15,15,4,8,1};
+	KMin km;
+	cout << km.kthMinimum(arr,1) << endl;
+	cout << km.kthMinimum(arr,2) << endl;
+	cout << km.kthMinimum(arr,5) << endl;
+	cout << km.kthMinimum(arr,7) << endl;
+	cout << km.kthMinimum(arr2,7) << endl;
+	cout << km.kthMinimum(arr2,4) << endl;
+	return 0;
+}
