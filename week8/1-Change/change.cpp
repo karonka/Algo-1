8fae0c74f55f58cf57ce171cf5666d19145b3fd4
karonka <karonka@abv.bg>
2015-07-30 13:14:42 +0300

    bug fixed

--- week8/1-Change/change.cpp
+++ week8/1-Change/change.cpp
@@ -4,21 +4,21 @@ using namespace std;
 int n,coins[7] = {1,2,5,10,20,50,100};
 int dp[1001][7] = {};
 
-int res(int aim, int max){
+int res(int aim, int max_i){
 	if (aim == 0) return 1;
 	if (aim < 0) return 0;
-	if (dp[aim][max]) return dp[aim][max];
+	if (dp[aim][max_i]) return dp[aim][max_i];
 	int sum = 0;
-	for(int i = 0; i < 7 && coins[i] <= max; i++){
-		sum += res(aim - coins[i], coins[i]);
+	for(int i = max_i; i >= 0; i--){
+		sum += res(aim - coins[i], i);
 	}
-	return dp[aim][max] = sum;
+	return dp[aim][max_i] = sum;
 }
 
 int find_max(){
 	int i = 6;
 	while(coins[i] > n) i--;
-	return coins[i];
+	return i;
 }
 
 int main(){
